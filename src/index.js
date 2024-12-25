dc9fdc8901adf184510b2838299e507709511ff7
abdeldjebar abbou <abbouabdeldjebar@gmail.com>
2024-12-25 20:09:42 +0100

    fix: export turkish translation

--- src/index.js
+++ src/index.js
@@ -5,3 +5,4 @@ export { default as esTrans } from "./translations/es.translations.json";
 export { default as frTrans } from "./translations/fr.translations.json";
 export { default as nlTrans } from "./translations/nl.translations.json";
 export { default as svTrans } from "./translations/sv.translations.json";
+export { default as trTrans } from "./translations/tr.translations.json";
